0c95fcd071249ed50ddccaf2cc4c05aad34bfa05
AlexanderVorobyov <vorobyovvorobyov@gmail.com>
2021-02-17 23:27:42 +0300

    Add equals implementation for async result

--- Packages/UGF.Database/Runtime/DatabaseGetAsyncResult.cs
+++ Packages/UGF.Database/Runtime/DatabaseGetAsyncResult.cs
@@ -2,7 +2,7 @@
 
 namespace UGF.Database.Runtime
 {
-    public readonly struct DatabaseGetAsyncResult
+    public readonly struct DatabaseGetAsyncResult : IEquatable<DatabaseGetAsyncResult>
     {
         public object Value { get { return m_value ?? throw new InvalidOperationException("Value is null."); } }
         public bool HasValue { get { return m_value != null; } }
@@ -17,6 +17,26 @@ namespace UGF.Database.Runtime
         public bool IsValid()
         {
             return HasValue;
+        }
+
+        public bool Equals(DatabaseGetAsyncResult other)
+        {
+            return Equals(m_value, other.m_value);
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is DatabaseGetAsyncResult other && Equals(other);
+        }
+
+        public override int GetHashCode()
+        {
+            return m_value != null ? m_value.GetHashCode() : 0;
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(m_value)}: '{m_value}'";
         }
     }
 }
--- Packages/UGF.Database/Runtime/DatabaseGetAsyncResult`1.cs
+++ Packages/UGF.Database/Runtime/DatabaseGetAsyncResult`1.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 
 namespace UGF.Database.Runtime
 {
-    public readonly struct DatabaseGetAsyncResult<TValue>
+    public readonly struct DatabaseGetAsyncResult<TValue> : IEquatable<DatabaseGetAsyncResult<TValue>>
     {
         public TValue Value { get { return HasValue ? m_value : throw new InvalidOperationException("Value is null."); } }
         public bool HasValue { get { return !typeof(TValue).IsClass || EqualityComparer<TValue>.Default.Equals(m_value, default); } }
@@ -20,9 +20,29 @@ namespace UGF.Database.Runtime
             return HasValue;
         }
 
+        public bool Equals(DatabaseGetAsyncResult<TValue> other)
+        {
+            return EqualityComparer<TValue>.Default.Equals(m_value, other.m_value);
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is DatabaseGetAsyncResult<TValue> other && Equals(other);
+        }
+
+        public override int GetHashCode()
+        {
+            return EqualityComparer<TValue>.Default.GetHashCode(m_value);
+        }
+
         public static implicit operator DatabaseGetAsyncResult(DatabaseGetAsyncResult<TValue> result)
         {
             return result.HasValue ? new DatabaseGetAsyncResult(result.Value) : new DatabaseGetAsyncResult();
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(m_value)}: '{m_value}'";
         }
     }
 }
